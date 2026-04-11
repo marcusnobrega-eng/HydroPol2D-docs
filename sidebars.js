9b7298238f205e48491438278c0b84cfa6a1bc1d
Marcus N. Gomes Jr. <marcusnobrega.engcivil@gmail.com>
2026-04-10 19:01:40 -0700

    Add files via upload

---
+++ sidebars.js
@@ -0,0 +1,59 @@
+// @ts-check
+
+/**
+ * @type {import('@docusaurus/plugin-content-docs').SidebarsConfig}
+ */
+const sidebars = {
+  tutorialSidebar: [
+    {
+      type: 'category',
+      label: 'Getting Started',
+      items: ['getting-started/overview'],
+    },
+    {
+      type: 'category',
+      label: 'Inputs',
+      items: [
+        'inputs/overview',
+        'inputs/dem',
+        'inputs/lulc',
+        'inputs/soil',
+        'inputs/dtb',
+        'inputs/lai',
+        'inputs/albedo',
+        'inputs/parameter-tables',
+        'inputs/optional-forcing',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Configuration',
+      items: [
+        'configuration/overview',
+        'configuration/general_data',
+        'configuration/flags',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'GEE Codes',
+      items: [
+        'GEE/overview',
+        'GEE/DailyForcing',
+      ],
+    },
+    {
+    type: 'category',
+    label: 'Background/Theory',
+    items: [
+      'theory/overview',
+      'theory/hydrologic-model',
+      'theory/hydrodynamic-model',
+      'theory/water-quality-model',
+      'theory/groundwater-model',
+    ],
+  },
+  ],
+};
+
+export default sidebars;
